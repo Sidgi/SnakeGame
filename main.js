9e4c9ab076b49144bf579c63f4fa936857cb84e3
Sidgi Samanduyev <samanduyev@gmail.com>
2019-04-02 10:49:08 -0400

    something

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
 let body = document.querySelector('body');
 let canvas = document.getElementById('mainCanvas');
 let ctx = canvas.getContext("2d");
-ctx.font = 'bold oblique 30px cursive';
-// ctx.strokeText('Hello world mypth',50,50);
-// ctx.fillStyle = 'red';
-// ctx.fillRect(100,400,150,150);
-// let img = document.getElementById('snakeImage');
-// ctx.drawImage(img,100,100);
+
 class Snakes{
-    constructor(x,y,size,speed,audio){
+    constructor(x,y,size,speed,color,length){
         this.x = x;
         this.y = y;
         this.size = size;
+        this.length = length;
+        this.color = color;
         this.speed = speed;
-        this.audio = audio
+        
+       
     }
     getX(){
        return this.x;
@@ -31,16 +29,38 @@ class Snakes{
         this.audio = music;
     }
     drawSnake(){
+    let count = 0;
+       for(let i = 1;i<this.length;i++){
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-        ctx.stroke();
+        ctx.fillstyle = this.color;
+        ctx.rect(this.x+count,this.y,this.size-3,this.size-3);
+        ctx.fill();
+        count+=30;
+        }
     }
     eat(){
 
     }
 }
+
 const drawSnake = () =>{
-    const mySnake = new Snakes(50,50,50,30,'snake.mp3');
+    let mySnake = new Snakes(50,50,30,3,"black",3);
     mySnake.drawSnake();
 }
+
+const moveTheSnake = (event)=>{
+    if(event.key === ('ArrowLeft'||'Left')){
+        mySnake.speed = -1;
+    }
+    else if(event.key === ('ArrowRight'|| 'Right') ){
+        mySnake.speed = 1;
+    }
+    else if(event.key === ('ArrowUp'||'Up')){
+        mySnake.speed = 1;
+    }
+    else if(event.key === ('ArrowDown'||'Down')){
+        mySnake.speed = -1;
+    }
+}
+//document.addEventListener('keydown',moveTheSnake);
 document.querySelector('button').addEventListener('click',drawSnake)
